36c3df5377aa0cf0d28b6b02a0378b6a60f62205
vklap <victor.klapholz@gmail.com>
2015-10-27 10:38:44 +0200

    TimerWrapper

---
+++ Utils/eToro.Utils.Timers.Tests/TimerWrapperShould.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Shouldly;
+
+namespace eToro.Utils.Timers.Tests
+{
+    [TestFixture]
+    public class TimerWrapperShould
+    {
+        private const double INTERVAL_IN_MILLISECONDS = 20;
+
+        private ITimerWrapper CreateTimer()
+        {
+            var timer = new TimerWrapper();
+            timer.Initialize(INTERVAL_IN_MILLISECONDS);
+            return timer;
+        }
+
+        [Test]
+        public void Start()
+        {
+            // Given
+            var timer = CreateTimer();
+            var counter = 0;
+            timer.Elapsed += (sender, args) =>
+            {
+                counter++;
+            };
+
+            // When
+            timer.Start();
+            Thread.Sleep(200);
+
+            // Then
+            counter.ShouldBeGreaterThan(0);
+        }
+
+        [Test]
+        public void Stop()
+        {
+            // Given
+            var timer = CreateTimer();
+            var counter = 0;
+            timer.Elapsed += (sender, args) =>
+            {
+                counter++;
+            };
+            timer.Start();
+            Thread.Sleep(200);
+            counter = 0;
+
+            // When
+            timer.Stop();
+            Thread.Sleep(200);
+
+
+            // Then
+            counter.ShouldBe(0);
+        }
+
+
+        [Test]
+        public void Dispose()
+        {
+            // Given
+            var timer = CreateTimer();
+            var counter = 0;
+            timer.Elapsed += (sender, args) =>
+            {
+                counter++;
+            };
+            timer.Start();
+            Thread.Sleep(200);
+            counter = 0;
+
+            // When
+            timer.Dispose();
+            Thread.Sleep(200);
+
+
+            // Then
+            counter.ShouldBe(0);
+        }
+    }
+}
---
+++ Utils/eToro.Utils.Timers/TimerWrapper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace eToro.Utils.Timers
+{
+    public class TimerWrapper : ITimerWrapper
+    {
+        private readonly Timer _timer = new Timer();
+
+        public void Start()
+        {
+            _timer.Enabled = true;
+        }
+
+        public void Stop()
+        {
+            _timer.Enabled = false;
+        }
+
+        public void Initialize(double interval)
+        {
+            _timer.Interval = interval;
+            _timer.Enabled = false;
+            _timer.Elapsed += (sender, args) =>
+            {
+                if (Elapsed != null)
+                {
+                    Elapsed(this, args);
+                }
+            };
+        }
+
+        public event ElapsedEventHandler Elapsed;
+
+        public void Dispose()
+        {
+            _timer.Dispose();
+        }
+    }
+}
